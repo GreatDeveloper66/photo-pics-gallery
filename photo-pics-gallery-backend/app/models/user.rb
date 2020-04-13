373b5662ed35d231f5cf19365b02793bbd1f7646
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-13 16:21:21 -0500

    Add liked_pictures and created_pictures relationships

--- photo-pics-gallery-backend/app/models/user.rb
+++ photo-pics-gallery-backend/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   has_many :likes
-  has_many :pictures, through: :likes
+  has_many :created_pictures, through: :likes, source: :picture
+  has_many :liked_pictures, through: :likes, source: :picture
 end
