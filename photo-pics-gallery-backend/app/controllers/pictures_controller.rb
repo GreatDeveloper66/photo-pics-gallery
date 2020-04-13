519969392232293e1197fcbe7ad55c8c6e9f5123
mpottebaum <mpottebaum@gmail.com>
2020-04-13 18:51:07 -0500

    setup pictures controller

--- photo-pics-gallery-backend/app/controllers/pictures_controller.rb
+++ photo-pics-gallery-backend/app/controllers/pictures_controller.rb
@@ -1,11 +1,16 @@
 class PicturesController < ApplicationController
 
     def create
+        picture = Picture.create(params[:picture])
+        render json: picture, include: :categories
     end
 
     def show
+        picture = Picture.find(params[:id])
+        render json: picture, include: :categories
     end
-    
+
     def destroy
+        Picture.find(params[:id]).destroy
     end
 end
