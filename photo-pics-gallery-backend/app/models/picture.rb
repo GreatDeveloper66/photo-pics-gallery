92f06984fceed345edfe2315dc40d152b1814146
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-13 15:40:19 -0500

    Add picture Associations

--- photo-pics-gallery-backend/app/models/picture.rb
+++ photo-pics-gallery-backend/app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ApplicationRecord
+  belongs_to :creator, class_name :"User"
+  has_many :picture_categorys
+  has_many :categorys through: :picture_categorys
+  has_many :likes
+  has_many :users, through: :likes
 end
