d5f8b895c2019c59c15d1c597fa88e65d7bfb6d0
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-14 16:29:59 -0500

    Move Register, Login and Profile forms to HTML

--- photo-pics-gallery-frontend/src/ashaffe.js
+++ photo-pics-gallery-frontend/src/ashaffe.js
@@ -3,7 +3,56 @@ $('.menu .item')
   .tab()
 ;
 
-document.getElementById('login-form').addEventListener('submit', function(event){
+const register_form =
+`
+<form class="ui form" id="register-form">
+  <div class="field">
+    <label>First Name</label>
+    <input type="text" name="firstname" placeholder="First Name">
+  </div>
+  <div class="field">
+    <label>Last Name</label>
+    <input type="text" name="lastname" placeholder="Last Name">
+  </div>
+  <button class="ui button" type="submit">Submit</button>
+</form>
+`;
+
+const login_form =
+`
+<form class="ui form" id="login-form">
+  <div class="field">
+    <label>First Name</label>
+    <input type="text" name="firstname" placeholder="First Name">
+  </div>
+  <div class="field">
+    <label>Last Name</label>
+    <input type="text" name="lastname" placeholder="Last Name">
+  </div>
+  <button class="ui button" type="submit">Submit</button>
+</form>
+`;
+
+const profile_form =
+`
+<form class="ui form" id="profile-form">
+  <div class="field">
+    <label>First Name</label>
+    <input type="text" name="firstname" placeholder="First Name">
+  </div>
+  <div class="field">
+    <label>Last Name</label>
+    <input type="text" name="lastname" placeholder="Last Name">
+  </div>
+  <button class="ui button" type="submit">Submit</button>
+</form>
+`;
+
+document.querySelectorAll('[data-tab="first"]')[1].innerHTML = register_form;
+document.querySelectorAll('[data-tab="second"]')[1].innerHTML = login_form;
+document.querySelectorAll('[data-tab="fourth"]')[1].innerHTML = profile_form
+
+document.getElementById('register-form').addEventListener('submit', function(event){
   event.preventDefault();
   const fullname = `${event.target.firstname.value} ${event.target.lastname.value}`;
   const configObj = {
