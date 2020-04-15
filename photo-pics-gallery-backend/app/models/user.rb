9b50d5b28f0273b420037f5d94f942b8c641fcd3
GreatDeveloper66 <ashaffe@gmail.com>
2020-04-15 17:05:56 -0500

    Link liked_picures method to route

--- photo-pics-gallery-backend/app/models/user.rb
+++ photo-pics-gallery-backend/app/models/user.rb
@@ -6,4 +6,13 @@ class User < ApplicationRecord
   def likes_picture(picture)
     Like.create(user_id:self.id,picture_id:picture.id);
   end
+
+  def users_likes
+    self.likes
+  end
+
+  def liked_pictures
+    self.users_likes.map {|like| like.picture}
+  end
+
 end
